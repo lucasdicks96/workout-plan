06b6f49ec8ead8244b8d0412dd3a77af83d68d32
Lucas <lucas1996dicks@gmail.com>
2024-08-05 00:50:51 +0200

    add style

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import styles from "./LandingPage.module.css";
+
+export default function Footer() {
+  const date = new Date().getFullYear();
+  return (
+    <>
+      <div className={styles.footer}>
+        <div className={styles.footer_content}>Copyright {date}</div>
+      </div>
+    </>
+  );
+}
