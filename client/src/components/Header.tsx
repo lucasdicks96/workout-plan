b7ae91054c457cdac5f00586c107d925ef404f8e
Lucas <lucas1996dicks@gmail.com>
2024-08-06 22:09:39 +0200

    add logout

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,15 +1,33 @@
-import styles from "./LandingPage.module.css";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import styles from "./LandingPage/LandingPage.module.css";
 
 export default function Header() {
+  const navigate = useNavigate();
+  const handleLogout = async () => {
+    try {
+      const response = await axios.get("http://localhost:5000/logout");
+      if (response.status === 200) {
+        navigate("/");
+      }
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <>
-      <div className={styles.header}>
-      <div className={styles.header_navbar}>
-        <ul>
-          <li>Login</li>
-          <li>Register</li>
-        </ul>
-      </div>
+      <div className="">
+        <div className={styles.header_navbar}>
+          <button
+            className={styles.button}
+            type="submit"
+            formMethod="GET"
+            onClick={handleLogout}
+          >
+            Logout
+          </button>
+        </div>
       </div>
     </>
   );
