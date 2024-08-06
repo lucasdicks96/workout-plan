9e134e3d2bdb7a784022e23b8628d1da27684281
Lucas <lucas1996dicks@gmail.com>
2024-08-06 22:11:17 +0200

    add form for login/register

---
+++ client/src/components/LandingPage/Form/Form.tsx
@@ -0,0 +1,117 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import styles form "./LoginForm.module.css";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import styles from "./Form.module.css";
+import stylesLandingPage from "../LandingPage.module.css";
+
+type FormState = {
+  email: string;
+  password: string;
+};
+
+interface FormProps {
+  route: string;
+  buttonName: string;
+}
+
+export default function Form({ route, buttonName }: FormProps) {
+  // const [route, buttonName] = props;
+  const [formState, setFormState] = useState<FormState>({
+    email: "",
+    password: "",
+  });
+  const navigate = useNavigate();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = e.target;
+    setFormState((prevState) => ({
+      ...prevState,
+      [id]: value,
+    }));
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Email: ", formState.email);
+    console.log("Password: ", formState.password);
+
+    try {
+      const response = await axios.post(
+        `http://localhost:5000/${route}`,
+        {
+          email: formState.email,
+          password: formState.password,
+          //   withCredentials: true,
+        },
+        {
+          headers: {
+            // "Access-Control-Allow-Origin": "*",
+            // "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+        }
+      );
+      if (response.status === 200) {
+        navigate("/dashboard");
+      }
+    } catch (error) {
+      console.error(`${buttonName} failed `, error);
+    }
+
+    setFormState({
+      email: "",
+      password: "",
+    });
+  };
+  return (
+    <div className={stylesLandingPage.body}>
+      <div className={stylesLandingPage.content}>
+        <div className={styles.formContainer}>
+          <form
+            className={styles.loginForm}
+            onSubmit={handleSubmit}
+            method="POST"
+          >
+            <h2>{buttonName}</h2>
+            <div className={styles.inputGroup}>
+              <label htmlFor="email">Username:</label>
+              <input
+                type="email"
+                id="email"
+                value={formState.email}
+                onChange={handleChange}
+                className={styles.input}
+                placeholder={"email"}
+                required
+              ></input>
+            </div>
+            <div className={styles.inputGroup}>
+              <label htmlFor="password">Passwort:</label>
+              <input
+                type="password"
+                id="password"
+                value={formState.password}
+                onChange={handleChange}
+                className={styles.input}
+                placeholder={"password"}
+                required
+              />
+            </div>
+
+            <div className={styles.buttonGroup}>
+              <button type="submit" className={stylesLandingPage.button}>
+                {buttonName}
+              </button>
+              <Link to="/" className={stylesLandingPage.button}>
+                Close
+              </Link>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
