de05709e1f73ca63ec7fc2f30d0f7361a6ceeb62
Lucas <lucas1996dicks@gmail.com>
2024-08-07 20:33:29 +0200

    add Link/Outlet

--- client/src/components/Dashboard/Dashboard.tsx
+++ client/src/components/Dashboard/Dashboard.tsx
@@ -1,15 +1,31 @@
 // import React from "react";
 // import ReactDom from "reac-dom/client";
-import Footer from "../Footer";
-import Header from "../Header";
-import LandingPagestyles from "../LandingPage/LandingPage.module.css";
+import { Link, Outlet } from "react-router-dom";
+// import Footer from "../Footer";
+import Header from "./Logout";
+// import stylesLandingPage from "../LandingPage/LandingPage.module.css";
+import styles from "./Dashboard.module.css";
 
 export default function Dashboard() {
   return (
-    <div className={LandingPagestyles.body}>
-      <Header />
-      <>DASHBOARD</>
-      <Footer />
+    <div className={styles.body}>
+      <div className={styles.content}>
+        <div className={styles.side}>
+          <Header />
+          <nav className={styles.menu}>
+            <Link to="exercises">Exercises</Link>
+            <Link to="workouts">Workouts</Link>
+            {/* <Link to="#">edit Exercise</Link> */}
+            {/* <Link to="#">edit Workout</Link> */}
+            {/* <Link to="#">Workouts</Link> */}
+            <Link to="history">Workout history</Link>
+          </nav>
+        </div>
+        <div className={styles.contentChild}>
+          <Outlet />
+        </div>
+      </div>
+      {/* <Footer /> */}
     </div>
   );
 }
