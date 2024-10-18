f78ac8a22442eae8eb7d1c408c06d829775b2358
Lucas <lucas1996dicks@gmail.com>
2024-10-18 23:00:49 +0200

    create passport config

---
+++ server/config/passport.ts
@@ -0,0 +1,65 @@
+import passport, { deserializeUser } from "passport";
+import { Strategy as LocalStrategy } from "passport-local";
+import bcrypt from "bcrypt";
+import pool from "./db";
+import userModel from "../types/userModel";
+// const LocalStrategy = passport.Strategy;
+
+passport.use(
+  "local",
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+    },
+    async (email: string, password: string, done: any) => {
+      const client = await pool.connect();
+      try {
+        const result = await client.query(
+          "SELECT * FROM users WHERE email = $1",
+          [email]
+        );
+        const user: userModel = result.rows[0];
+
+        if (!user) {
+          return done(null, false, { message: "User not found" });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+          return done(null, false, { message: "Wrong password" });
+        }
+        return done(null, user);
+      } catch (err) {
+        return done(err);
+      } finally {
+        client.release();
+      }
+    }
+  )
+);
+
+passport.serializeUser(function (user: any, done: any) {
+  return done(null, user.id);
+});
+
+passport.deserializeUser(async function (id: number, done: any) {
+  const client = await pool.connect();
+  try {
+    const result = await client.query("SELECT * FROM users WHERE id = $1", [
+      id,
+    ]);
+    if (result.rows.length > 0) {
+      const user = result.rows[0];
+      done(null, user);
+    } else {
+      done(new Error("User not found"), null);
+    }
+  } catch (err) {
+    done(err);
+  } finally {
+    client.release();
+  }
+});
+
+export default passport;
