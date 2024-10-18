b32da6d20951abcd443a326ec9cb302483c75d9d
Lucas <lucas1996dicks@gmail.com>
2024-10-18 22:51:47 +0200

    delete old code and reworked auth req

--- client/src/components/Dashboard/Dashboard.tsx
+++ client/src/components/Dashboard/Dashboard.tsx
@@ -1,31 +1,56 @@
-// import React from "react";
-// import ReactDom from "reac-dom/client";
-import { Link, Outlet } from "react-router-dom";
-// import Footer from "../Footer";
-import Header from "./Logout";
-// import stylesLandingPage from "../LandingPage/LandingPage.module.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link, Outlet, useNavigate } from "react-router-dom";
 import styles from "./Dashboard.module.css";
+import Header from "./Logout";
 
 export default function Dashboard() {
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+  const navigate = useNavigate();
+
+  // // useEffect-Hook zur Ausführung des Authentifizierungschecks beim Mounten des Components
+  useEffect(() => {
+    checkAuth();
+  }, []);
+
+  async function checkAuth() {
+    try {
+      const response = await axios.get("http://localhost:5000/users/users", {
+        withCredentials: true,
+      });
+
+      if (response.status === 200 || response.status === 201) {
+        console.log(response.data);
+        setIsAuthenticated(true);
+      }
+    } catch (err) {
+      console.error("Not logged in!");
+      setIsAuthenticated(false);
+      navigate("/");
+    }
+  }
+
   return (
     <div className={styles.body}>
       <div className={styles.content}>
-        <div className={styles.side}>
-          <Header />
-          <nav className={styles.menu}>
-            <Link to="exercises">Exercises</Link>
-            <Link to="workouts">Workouts</Link>
-            {/* <Link to="#">edit Exercise</Link> */}
-            {/* <Link to="#">edit Workout</Link> */}
-            {/* <Link to="#">Workouts</Link> */}
-            <Link to="history">Workout history</Link>
-          </nav>
-        </div>
-        <div className={styles.contentChild}>
-          <Outlet />
-        </div>
+        {isAuthenticated ? (
+          <>
+            <div className={styles.side}>
+              <Header />
+              <nav className={styles.menu}>
+                <Link to="exercises">Exercises</Link>
+                <Link to="workouts">Workouts</Link>
+                <Link to="history">Workout history</Link>
+              </nav>
+            </div>
+            <div className={styles.contentChild}>
+              <Outlet />
+            </div>
+          </>
+        ) : (
+          "Loading..."
+        )}
       </div>
-      {/* <Footer /> */}
     </div>
   );
 }
