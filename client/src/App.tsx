856213a707cd91161fbe5c7b7ececf9968c33acb
Lucas <lucas1996dicks@gmail.com>
2025-01-26 15:27:21 +0100

    update routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,53 +4,17 @@ import Form from "./components/landingPage/form/Form";
 import LandingPage from "./components/landingPage/LandingPage";
 // import RegisterForm from "./components/LandingPage/Register/RegisterForm";
 import Dashboard from "./components/dashboard/Dashboard";
-import Exercises from "./components/exercises/Exercises";
 import CreateExercises from "./components/exercises/CreateExercises";
 import EditExercises from "./components/exercises/EditExercises";
+import Exercises from "./components/exercises/Exercises";
 import NotFound from "./components/NotFound";
-import History from "./components/workouts/History";
-import Workouts from "./components/workouts/Workouts";
 import CreateWorkouts from "./components/workouts/CreateWorkouts";
 import EditWorkouts from "./components/workouts/EditWorkouts";
+import History from "./components/workouts/History";
+import Workouts from "./components/workouts/Workouts";
 // import './App.css'
 
 function App() {
-  // const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
-  // const navigate = useNavigate();
-  // useEffect(() => {
-  //   const loggedIn = async () => {
-  //     try {
-  //       const result = await axios.get("http://localhost:5000/dashboard");
-  //       console.log(result);
-  //       if (result.status === 200) {
-  //         setIsLoggedIn(true);
-  //       } else {
-  //         setIsLoggedIn(false);
-  //       }
-  //     } catch (error) {
-  //       setIsLoggedIn(false);
-  //       navigate("/");
-  //       console.error("Not logged in", error);
-  //     }
-  //   };
-
-  //   loggedIn(); // Call the async function to check authentication
-  // }, [navigate]); // Empty dependency array ensures the effect runs once
-  // const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
-  // async function checkAuth() {
-  //   try {
-  //     const check = await axios.get("http://localhost:5000/auth");
-  //     console.log("check status", check.data);
-  //     if (check.status === 200) setIsAuthenticated(true);
-  //     else setIsAuthenticated(false);
-  //   } catch (error) {
-  //     console.error("auth error", error);
-  //   }
-  // }
-  // useEffect(() => {
-  //   checkAuth();
-  // });
-
   return (
     <BrowserRouter>
       <Routes>
@@ -63,21 +27,17 @@ function App() {
           path="/register"
           element={<Form route="register" buttonName="Register" />}
         />
-
-        {/* <Route path="/dashboard" element={<ProtectedRoute />}> */}
-        {/* <Route index element={<Dashboard />} /> */}
-        {/* <Route element={<ProtectedRoute isAuthenticated={isAuthenticated} />}> */}
         <Route path="/users" element={<Dashboard />}>
-          <Route path="/users/exercises" element={<Exercises />}>
+          <Route path="exercises" element={<Exercises />}>
             <Route path="edit-exercises" element={<EditExercises />} />
             <Route path="create-exercises" element={<CreateExercises />} />
           </Route>
-          <Route path="workouts" element={<Workouts />} />
-          <Route path="edit-workouts" element={<EditWorkouts />} />
-          <Route path="create-workouts" element={<CreateWorkouts />} />
+          <Route path="workouts" element={<Workouts />}>
+            <Route path="edit-workouts" element={<EditWorkouts />} />
+            <Route path="create-workouts" element={<CreateWorkouts />} />
+          </Route>
           <Route path="history" element={<History />} />
         </Route>
-        {/* </Route> */}
 
         <Route path="*" element={<NotFound />} />
       </Routes>
