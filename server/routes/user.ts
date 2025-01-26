1674fb41c49fb929d4751395dc3c0da8a4dc3258
Lucas <lucas1996dicks@gmail.com>
2025-01-26 18:27:38 +0100

    modified login route and set user id in cookie

--- server/routes/user.ts
+++ server/routes/user.ts
@@ -1,7 +1,8 @@
-import { Router, Request, Response, NextFunction } from "express";
-import passport from "passport";
 import bcrypt from "bcrypt";
+import { NextFunction, Request, Response, Router } from "express";
+import passport from "passport";
 import pool from "../config/db";
+import userModel from "../types/userModel";
 
 const router = Router();
 
@@ -32,16 +33,56 @@ router.post("/register", async (req: Request, res: Response) => {
   }
 });
 
-router.post(
-  "/login",
-  passport.authenticate("local", {
-    successRedirect: "/user/users",
-    failureRedirect: "/login",
-  })
-);
+// router.post(
+//   "/login",
+//   passport.authenticate("local", {
+//     successRedirect: "/user/users",
+//     failureRedirect: "/login",
+//   })
+// );
+
+router.post("/login", (req: Request, res: Response, next: NextFunction) => {
+  passport.authenticate("local", (err: any, user: any, info: any) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return res
+        .status(401)
+        .json({ message: info?.message || "Invalid credentials" });
+    }
+
+    req.logIn(user, (err) => {
+      if (err) {
+        return next(err);
+      }
+
+      // User-ID in der Session speichern (falls du es dort auch brauchst)
+      req.session.sessionUser = { id: user.id, email: user.email };
+
+      // User-ID in das Cookie setzen
+      res.cookie("userId", user.id, {
+        httpOnly: true, // Verhindert den Zugriff durch JavaScript im Browser
+        secure: false, // Setze es auf `true` in der Produktion, wenn HTTPS verwendet wird
+        maxAge: 1000 * 60 * 30, // Cookie-Gültigkeit (hier 30 Minuten)
+        sameSite: "strict", // Verhindert Cross-Site Request Forgery
+      });
+
+      return res.status(200).json({ message: "Login successful", user });
+    });
+  })(req, res, next);
+});
 
 router.get("/users", isAuthenticated, (req: Request, res: Response) => {
   res.json({ message: "Willkommen" });
+});
+
+router.get("/id", (req: Request, res: Response) => {
+  if (req.cookies.userId) {
+    return res.json({ uid: req.cookies.userId });
+  } else {
+    return res.status(400).json({ message: "User not authenticated" });
+  }
 });
 
 router.post("/logout", (req: Request, res: Response) => {
