72288f68cf1566969bc0d93dabb6de6159c0b33d
Lucas Dicks <lucas1996dicks@gmail.com>
2025-06-25 00:20:52 +0200

    add test array for storage

--- server/routes/workout.ts
+++ server/routes/workout.ts
@@ -1,27 +1,8 @@
-import exp from "constants";
-import { Router, Request, Response } from "express";
+import { Request, Response, Router } from "express";
 
 const router = Router();
 
 router.get("/all-workouts", (req: Request, res: Response) => {
-  const workouts = [
-    {
-      id: 1,
-      name: "Full Body Workout",
-      description: "A complete workout for all muscle groups.",
-    },
-    {
-      id: 2,
-      name: "Cardio Blast",
-      description: "High-intensity cardio workout.",
-    },
-    {
-      id: 3,
-      name: "Strength Training",
-      description: "Focus on building muscle strength.",
-    },
-  ];
-
   res.json({ workouts }).status(200);
 });
 
@@ -30,6 +11,11 @@ router.get("/workout/:id", (req: Request, res: Response) => {});
 router.post("/create-workout", async (req: Request, res: Response) => {
   console.log(req.body);
   // Here you would typically save the workout to a database
+  workouts.push({
+    id: workouts.length + 1,
+    title: req.body.title,
+    description: req.body.description,
+  });
   res.status(201).json({
     message: "Workout created successfully",
     workout: req.body,
@@ -37,3 +23,21 @@ router.post("/create-workout", async (req: Request, res: Response) => {
 });
 
 export default router;
+
+const workouts = [
+  {
+    id: 1,
+    title: "Full Body Workout",
+    description: "A complete workout for all muscle groups.",
+  },
+  {
+    id: 2,
+    title: "Cardio Blast",
+    description: "High-intensity cardio workout.",
+  },
+  {
+    id: 3,
+    title: "Strength Training",
+    description: "Focus on building muscle strength.",
+  },
+];
