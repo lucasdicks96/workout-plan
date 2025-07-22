5e5309a42d7cba48f5a322131bab5247367999d3
Lucas Dicks <lucas1996dicks@gmail.com>
2025-07-22 20:34:29 +0200

    new routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,47 +1,77 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import ProtectedRoute from "./Router/ProtectedRoute";
-import Form from "./components/landingPage/form/Form";
-import LandingPage from "./components/landingPage/LandingPage";
-// import RegisterForm from "./components/LandingPage/Register/RegisterForm";
-import Dashboard from "./components/dashboard/Dashboard";
-import CreateExercises from "./components/exercises/CreateExercises";
-import EditExercises from "./components/exercises/EditExercises";
-import ExercisesList from "./components/Exercises/ExercisesList";
-import NotFound from "./components/NotFound";
-import CreateWorkouts from "./components/workouts/CreateWorkouts";
-import EditWorkouts from "./components/workouts/EditWorkouts";
-import History from "./components/workouts/History";
-import Workouts from "./components/workouts/Workouts";
-// import './App.css'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useAuth } from "./context/AuthContext";
+import DashboardLayout from "./components/Layout/DashboardLayout";
+import AuthPage from "./components/LandingPage/AuthPage";
+import Exercise from "./components/Exercises/ExercisesList";
+import "./styles/global.css";
+import CreateExercise from "./components/Exercises/CreateExercises";
+import EditExercise from "./components/Exercises/EditExercises";
+
+const Dashboard = () => (
+  <div>
+    <h1 className="pageTitle">Dashboard</h1>
+  </div>
+);
+// const ExercisesList = () => (
+//   <div>
+//     <h1 className="pageTitle">Übungen</h1>
+//   </div>
+// );
+const Workouts = () => (
+  <div>
+    <h1 className="pageTitle">Workouts</h1>
+  </div>
+);
+const History = () => (
+  <div>
+    <h1 className="pageTitle">Verlauf</h1>
+  </div>
+);
+const NotFound = () => (
+  <div>
+    <h1 className="pageTitle">404 - Nicht gefunden</h1>
+  </div>
+);
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route
-          path="/login"
-          element={<Form route="login" buttonName="Login" />}
-        />
-        <Route
-          path="/register"
-          element={<Form route="register" buttonName="Register" />}
-        />
-        <Route path="/users" element={<Dashboard />}>
-          <Route path="exercises" element={<ExercisesList />}>
-            <Route path="edit-exercises" element={<EditExercises />} />
-            <Route path="create-exercises" element={<CreateExercises />} />
-          </Route>
-          <Route path="workouts" element={<Workouts />}>
-            <Route path="edit-workouts" element={<EditWorkouts />} />
-            <Route path="create-workouts" element={<CreateWorkouts />} />
-          </Route>
-          <Route path="history" element={<History />} />
-        </Route>
+  const { user, loading, theme } = useAuth();
+  // console.log("App.tsx user:", user);
 
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+  if (loading) {
+    return <div className={`app-container ${theme} authPage`}>Lade...</div>;
+  }
+
+  return (
+    <div className={`app-container ${theme}`}>
+      <BrowserRouter>
+        <Routes>
+          {user ? (
+            <Route path="/" element={<DashboardLayout />}>
+              <Route index element={<Navigate to="/dashboard" />} />
+              <Route path="dashboard" element={<Dashboard />} />
+              <Route path="exercises" element={<Exercise />} />
+              <Route
+                path="exercises/edit-exercises"
+                element={<EditExercise />}
+              />
+              <Route
+                path="exercises/create-exercises"
+                element={<CreateExercise />}
+              />
+              <Route path="workouts" element={<Workouts />} />
+              <Route path="history" element={<History />} />
+            </Route>
+          ) : (
+            <>
+              <Route path="/login" element={<AuthPage />} />
+              <Route path="/register" element={<AuthPage isRegister />} />
+              <Route path="*" element={<Navigate to="/login" />} />
+            </>
+          )}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
