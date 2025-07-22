fbe6b1d822ea669f94cf9fd0e480a5d7fbaca662
Lucas Dicks <lucas1996dicks@gmail.com>
2025-07-22 20:36:45 +0200

    update list and card components

--- client/src/components/Exercises/ExercisesList.tsx
+++ client/src/components/Exercises/ExercisesList.tsx
@@ -1,99 +1,135 @@
-import { useEffect, useState } from "react";
-import { Outlet, useLocation, useNavigate } from "react-router-dom";
-import { fetchAllExercises } from "../../api/fetchExercise";
-import { IExercisesList } from "../../types/exercises";
-import stylesDashboard from "../dashboard/Dashboard.module.css";
-import styles from "./ExercisesList.module.css";
-// import image from "../../assets/BackgroundImage.png";
-// const img: string = image;
+import { memo, useEffect, useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
+import { apiService } from "../../services/apiService";
+import styles from "../../styles/Exercises.module.css";
+import stylesLayout from "../../styles/Layout.module.css";
+import { CombinedExercise } from "../../types/exercises";
 
-export default function Exercise() {
-  const [exerciseList, setExerciseList] = useState<IExercisesList[]>([]);
+export default function Exercises() {
+  const [exerciseList, setExerciseList] = useState<CombinedExercise[]>([]);
   const [isLoading, setIsLoading] = useState(true);
   const navigate = useNavigate();
-  const location = useLocation();
-  const isSubRoute =
-    location.pathname.includes("edit-exercises") ||
-    location.pathname.includes("create-exercises");
+
+  const { user } = useAuth();
+  const id = useRef<number>(0);
 
   useEffect(() => {
-    const loadAllExercises = async () => {
+    async function loadAllExercises() {
       try {
-        const exercises = await fetchAllExercises();
-        setExerciseList(exercises);
+        if (!user || user.id === undefined || user.id === null) {
+          console.error("Benutzer ist nicht angemeldet oder hat keine ID.");
+          return;
+        }
+        id.current = user.id;
+        const response = await apiService.getAllExercises(id.current);
+        // const exercises: ExercisesList[] = response.data.exercises;
+        setExerciseList(response.data.exercises);
       } catch (error) {
         console.error("Fehler beim Abrufen der Übungen:", error);
       } finally {
         setIsLoading(false);
       }
-    };
+    }
     loadAllExercises();
-  }, [location]);
-
-  function ExerciseList() {
-    if (isLoading) {
-      return <p>Lade Übungen...</p>;
-    }
-
-    if (exerciseList.length === 0) {
-      return <p>Keine Übungen verfügbar.</p>;
-    }
-    return (
-      <div className={styles.exerciseList}>
-        {exerciseList.map((item) => (
-          <ExerciseCard
-            key={item.id}
-            id={item.id}
-            title={item.title}
-            description={item.description}
-            img_path={item.img_path}
-          />
-        ))}
-      </div>
-    );
-  }
-  function ExerciseCard({ title, description, img_path }: IExercisesList) {
-    return (
-      <div className={styles.exerciseCardContainer}>
-        <div className={styles.exerciseImageContainer}>
-          <a className={styles.exerciseImage}>
-            <img src={img_path} />
-          </a>
-        </div>
-        <div className={styles.exerciseCardBody}>
-          <h3>{title}</h3>
-          <div className={styles.exerciseCardDescription}>{description}</div>
-        </div>
-      </div>
-    );
-  }
+  }, [user]);
 
   return (
     <>
-      {!isSubRoute && (
-        <>
-          <h2>Exercises</h2>
-          <div className={stylesDashboard.content}>
-            <ExerciseList />
-          </div>
-          <div>
-            <button
-              className={styles.button}
-              onClick={() => navigate("edit-exercises")}
-            >
-              Edit Exercise
-            </button>
-            <button
-              className={styles.button}
-              onClick={() => navigate("create-exercises")}
-            >
-              Create Exercise
-            </button>
-          </div>
-        </>
-      )}
-
-      <Outlet />
+      <div className="content">
+        <h2 className={stylesLayout.pageTitle}>Übungen</h2>
+        <ExerciseList
+          exerciseList={exerciseList}
+          isLoading={isLoading}
+          userId={id.current}
+        />
+      </div>
+      <div className="button-container">
+        <button className="button" onClick={() => navigate("edit-exercises")}>
+          Bearbeiten
+        </button>
+        <button className="button" onClick={() => navigate("create-exercises")}>
+          Erstellen
+        </button>
+      </div>
     </>
   );
 }
+
+type ExerciseProps = {
+  isLoading: boolean;
+  exerciseList: CombinedExercise[];
+  userId: number;
+};
+export function ExerciseList({
+  isLoading,
+  exerciseList,
+  userId,
+}: ExerciseProps) {
+  const [selectedExercise, setSelectedExercise] =
+    useState<CombinedExercise | null>(null);
+
+  const location = window.location.pathname;
+  const isEditPage = location.includes("edit-exercises");
+
+  const handleCardClick = (exercise: CombinedExercise) => {
+    if (exercise.isUserCreated && isEditPage) {
+      setSelectedExercise(exercise);
+    }
+  };
+
+  const handleCloseModal = () => {
+    setSelectedExercise(null);
+  };
+
+  if (isLoading) {
+    return <p>Lade Übungen...</p>;
+  }
+
+  if (exerciseList.length === 0) {
+    return <p>Keine Übungen verfügbar.</p>;
+  }
+  return (
+    <>
+      <div className={styles.exerciseList}>
+        {exerciseList.map((item) => (
+          <ExerciseCard
+            key={item.compositeKey}
+            title={item.title}
+            description={item.description}
+            isUserCreated={item.isUserCreated}
+            compositeKey={item.compositeKey}
+            originalId={item.originalId}
+            onClick={() => handleCardClick(item)}
+          />
+        ))}
+      </div>
+      {selectedExercise && (
+        <Modal
+          isOpen={!!selectedExercise}
+          onClose={handleCloseModal}
+          exerciseData={selectedExercise}
+          userId={userId}
+        />
+      )}
+    </>
+  );
+}
+
+import Modal from "../Modal";
+
+type ExerciseCardProps = CombinedExercise & {
+  onClick: () => void;
+};
+
+const ExerciseCard = memo(
+  ({ title, description, isUserCreated, onClick }: ExerciseCardProps) => {
+    return (
+      <div className="card" onClick={onClick}>
+        {isUserCreated && <span>(Eigene Übung)</span>}
+        <h3>{title}</h3>
+        <div className={styles.exerciseCardDescription}>{description}</div>
+      </div>
+    );
+  }
+);
