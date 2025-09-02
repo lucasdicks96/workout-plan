f90a4f69359235afb9b1763610f2b507065ce5d7
Lucas Dicks <lucas1996dicks@gmail.com>
2025-09-03 00:17:34 +0200

    add workoutmanager hook and some logic

--- client/src/components/Workouts/EditWorkouts.tsx
+++ client/src/components/Workouts/EditWorkouts.tsx
@@ -1,29 +1,54 @@
 import { useCallback, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
+import { useWorkoutManager } from "../../hooks/useWorkoutManager";
 import { apiService } from "../../services/apiService";
 import stylesLayout from "../../styles/Layout.module.css";
-import {
-  Workout,
-  WorkoutExercises as WorkoutExercisesType,
-} from "../../types/workouts";
-import { WorkoutList as WorkoutPlans } from "./Workouts";
+import { CombinedExercise } from "../../types/exercises";
+import { Workout } from "../../types/workouts";
+import ExerciseSelectionList from "../Exercises/ExerciseSelectionList";
 import WorkoutExercises from "./WorkoutExercises";
+import { WorkoutList as WorkoutPlans } from "./Workouts";
+
 export default function EditWorkouts() {
   const [workoutPlans, setWorkoutPlans] = useState<Workout[]>([]);
-  const [workoutExercises, setWorkoutExercises] = useState<
-    WorkoutExercisesType[]
-  >([]);
+  const [allExercises, setAllExercises] = useState<CombinedExercise[]>([]);
   const [selectedWorkoutId, setSelectedWorkoutId] = useState<number | null>(
     null
   );
   const [workoutName, setWorkoutName] = useState<string>("");
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
   // const [open, setOpen] = useState<boolean>(false);
-  // const uid = useRef(0);
+  // const workoutId = useRef(selectedWorkoutId);
   const { user } = useAuth();
 
+  const {
+    workoutList,
+    setWorkoutList,
+    isSelecting,
+    setIsSelecting,
+    updateExerciseInWorkout,
+    handleAddSet,
+    handleRemoveSet,
+    removeExerciseFromWorkout,
+    addExerciseToWorkout,
+  } = useWorkoutManager();
+
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchAllExercises = async () => {
+      try {
+        if (!user || user.id === undefined || user.id === null) return;
+        const response = await apiService.getAllExercises(user.id);
+        setAllExercises(response.data.exercises);
+      } catch (error) {
+        console.error("Fehler beim Abrufen aller Übungen:", error);
+      }
+    };
+    fetchAllExercises();
+  }, [user]);
 
   const loadAllWorkouts = useCallback(async () => {
     setIsLoading(true);
@@ -42,49 +67,62 @@ export default function EditWorkouts() {
   }, [loadAllWorkouts]);
 
   const loadExercises = useCallback(async () => {
+    if (!user || selectedWorkoutId === null) return;
     setIsLoading(true);
     try {
-      if (!user || user.id === undefined || user.id === null) {
-        console.error("User is not logged in or does not have an ID.");
-        return;
-      }
-      if (selectedWorkoutId === null) {
-        return;
-      }
       const response = await apiService.getWorkoutExercises(
         selectedWorkoutId,
         user.id
       );
-      console.log(response.data);
-      setWorkoutExercises(response.data.exercises);
+      setWorkoutList(response.data.exercises);
       setWorkoutName(response.data.title);
     } catch (error) {
       console.error("Fehler beim Laden der Workout-Übungen:", error);
     } finally {
       setIsLoading(false);
     }
-  }, [selectedWorkoutId, user]);
+  }, [selectedWorkoutId, user, setWorkoutList]);
 
   useEffect(() => {
-    loadExercises();
-  }, [loadExercises]);
+    if (selectedWorkoutId !== null) {
+      loadExercises();
+    } else {
+      setWorkoutList([]);
+      setWorkoutName("");
+    }
+  }, [selectedWorkoutId, loadExercises, setWorkoutList]);
 
-  const updateExerciseInWorkout = (
-    key: string,
-    field: keyof WorkoutExercisesType,
-    value: string
-  ) => {
-    setWorkoutExercises((current) =>
-      current.map((ex) =>
-        ex.compositeKey === key ? { ...ex, [field]: value } : ex
-      )
-    );
-  };
+  const handleSaveWorkout = async () => {
+    try {
+      if (!user || selectedWorkoutId === null) {
+        setError("Benutzer ID oder Trainingsplan-ID fehlt.");
+        throw new Error();
+      }
 
-  const removeExerciseFromWorkout = (key: string) => {
-    setWorkoutExercises((current) =>
-      current.filter((ex) => ex.compositeKey !== key)
-    );
+      if (workoutName.trim().length === 0) {
+        setError("Trainingsname darf nicht leer sein.");
+        throw new Error("Trainingsname darf nicht leer sein.");
+      }
+
+      if (workoutList.length === 0) {
+        setError("Trainingsplan muss mindestens eine Übung enthalten.");
+        throw new Error("Trainingsplan muss mindestens eine Übung enthalten.");
+      }
+
+      if (workoutList.filter((ex) => ex.sets.length === 0).length > 0) {
+        setError("Jede Übung muss mindestens einen Satz enthalten.");
+        throw new Error("Jede Übung muss mindestens einen Satz enthalten.");
+      }
+      const response = await apiService.updateWorkout(
+        workoutName,
+        user.id,
+        selectedWorkoutId!,
+        workoutList
+      );
+      if (response.status === 200) navigate("/workouts");
+    } catch (error) {
+      console.error("Fehler beim Speichern des Trainingsplans:", error);
+    }
   };
 
   const handlePlanSelect = (workoutId: number) => {
@@ -94,32 +132,24 @@ export default function EditWorkouts() {
   const handleBackToList = () => {
     setSelectedWorkoutId(null);
   };
-  const handleAddSet = ({ compositeKey }: { compositeKey: string }) => {
-    const index = workoutExercises.findIndex(
-      (ex) => ex.compositeKey === compositeKey
+
+  if (isSelecting) {
+    return (
+      <ExerciseSelectionList
+        allExercises={allExercises}
+        onBack={() => setIsSelecting(false)}
+        workoutList={workoutList}
+        onSelectExercise={addExerciseToWorkout}
+      />
     );
-    if (index !== -1) {
-      workoutExercises[index].sets.push({
-        setNumber: 0,
-        repetitions: 0,
-        weight: 0,
-      });
-    }
-    console.log(compositeKey);
-  };
-
-  const handleRemoveSet = () => {};
-
-  // const confirmModifiedExercises = (workoutId: number, userId: number) => {
-  //   try {
-  //   } catch (error) {}
-  // };
+  }
 
   return (
     <div className="content">
       <h2 className={stylesLayout.pageTitle}>Edit Workout</h2>
       {selectedWorkoutId !== null ? (
         <>
+          {error && <p style={{ color: "var(--c-danger)" }}>{error}</p>}
           <input
             className="input"
             style={{ maxWidth: "20rem" }}
@@ -131,8 +161,13 @@ export default function EditWorkouts() {
           <WorkoutExercises
             onBack={handleBackToList}
             onRemove={removeExerciseFromWorkout}
-            onUpdate={updateExerciseInWorkout}
-            workoutList={workoutExercises}
+            onUpdate={(key, setIndex, field, value) => {
+              const numericValue = Number(value);
+              if (!isNaN(numericValue)) {
+                updateExerciseInWorkout(key, setIndex, field, numericValue);
+              }
+            }}
+            workoutList={workoutList}
             onAddSet={handleAddSet}
             onRemoveSet={handleRemoveSet}
           />
@@ -143,7 +178,12 @@ export default function EditWorkouts() {
             >
               Zurück
             </button>
-            <button className="button">Bestätigen</button>
+            <button className="button" onClick={() => setIsSelecting(true)}>
+              Hinzufügen
+            </button>
+            <button className="button" onClick={handleSaveWorkout}>
+              Bestätigen
+            </button>
           </div>
         </>
       ) : (
