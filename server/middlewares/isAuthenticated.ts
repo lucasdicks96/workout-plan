001292f707ed5ca4f1d1677424a9a0999356f55f
Lucas Dicks <lucas1996dicks@gmail.com>
2025-09-16 18:49:07 +0200

    init

---
+++ server/middlewares/isAuthenticated.ts
@@ -0,0 +1,14 @@
+import { Request, Response, NextFunction } from "express";
+
+export function isAuthenticated(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.clearCookie("userId", { path: "/" });
+  res.clearCookie("connect.sid", { path: "/" });
+  res.status(401).json({ message: "Nicht authentifiziert" });
+}
