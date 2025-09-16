083e8547e6c606fd363ff2629a594c08979699a9
Lucas Dicks <lucas1996dicks@gmail.com>
2025-09-16 23:50:05 +0200

    remove try-catch-blocks

--- server/repositories/user.repository.ts
+++ server/repositories/user.repository.ts
@@ -2,40 +2,28 @@ import pool from "../config/db";
 import User, { UserWithoutPassword } from "../types/user.types";
 
 export async function findUserById(id: string): Promise<User> {
-  try {
-    const result = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
-    const user: User = result.rows[0];
-    return user;
-  } catch (dbError) {
-    throw dbError;
-  }
+  const result = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+  const user: User = result.rows[0];
+  return user;
 }
 
 export async function findUserByEmail(email: string): Promise<User> {
-  try {
-    const result = await pool.query("SELECT * FROM users WHERE email = $1", [
-      email,
-    ]);
+  const result = await pool.query("SELECT * FROM users WHERE email = $1", [
+    email,
+  ]);
 
-    const user: User = result.rows[0];
-    return user;
-  } catch (dbError) {
-    throw dbError;
-  }
+  const user: User = result.rows[0];
+  return user;
 }
 
 export async function createUser(
   email: string,
   hashedPassword: string
 ): Promise<UserWithoutPassword> {
-  try {
-    const result = await pool.query(
-      "INSERT INTO users (email, password) VALUES ($1, $2) RETURNING id, email, role",
-      [email, hashedPassword]
-    );
-    const newUser: UserWithoutPassword = result.rows[0];
-    return newUser;
-  } catch (dbError) {
-    throw dbError;
-  }
+  const result = await pool.query(
+    "INSERT INTO users (email, password) VALUES ($1, $2) RETURNING id, email, role",
+    [email, hashedPassword]
+  );
+  const newUser: UserWithoutPassword = result.rows[0];
+  return newUser;
 }
