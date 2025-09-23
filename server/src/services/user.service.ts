da794d79101dbcfb46f54e799c6c19a0a72cf2e2
Lucas Dicks <lucas1996dicks@gmail.com>
2025-09-24 00:09:33 +0200

    add try/catch

--- server/src/services/user.service.ts
+++ server/src/services/user.service.ts
@@ -1,9 +1,23 @@
-import { hashPassword } from "./auth.service";
+import { DatabaseError } from "pg";
 import * as userRepository from "../repositories/user.repository";
+import {
+  ConflictError,
+  InternalServerError
+} from "../types/errors.types";
+import { hashPassword } from "./auth.service";
 
 export async function createUser(email: string, password: string) {
-  const hashedPassword = await hashPassword(password);
-  const user = await userRepository.createUser(email, hashedPassword);
+  try {
+    const hashedPassword = await hashPassword(password);
+    const user = await userRepository.createUser(email, hashedPassword);
 
-  return user;
+    return user;
+  } catch (error) {
+    if (error instanceof DatabaseError) {
+      if (error.code === "23505") {
+        throw new ConflictError("Benutzer mit dieser E-Mail existiert bereits");
+      }
+    }
+    throw new InternalServerError("Fehler beim Erstellen des Benutzers");
+  }
 }
