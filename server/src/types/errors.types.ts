b2f85cc0744aa6254178a82a66a075a4b0053c2f
Lucas Dicks <lucas1996dicks@gmail.com>
2025-09-24 00:09:04 +0200

    add ConflictError

--- server/src/types/errors.types.ts
+++ server/src/types/errors.types.ts
@@ -2,7 +2,7 @@ export class NotFoundError extends Error {
   statusCode: number;
   constructor(message: string) {
     super(message);
-    this.name = 'NotFoundError';
+    this.name = "NotFoundError";
     this.statusCode = 404;
   }
 }
@@ -11,7 +11,7 @@ export class BadRequestError extends Error {
   statusCode: number;
   constructor(message: string) {
     super(message);
-    this.name = 'BadRequestError';
+    this.name = "BadRequestError";
     this.statusCode = 400;
   }
 }
@@ -20,7 +20,7 @@ export class UnauthorizedError extends Error {
   statusCode: number;
   constructor(message: string) {
     super(message);
-    this.name = 'UnauthorizedError';
+    this.name = "UnauthorizedError";
     this.statusCode = 401;
   }
 }
@@ -29,7 +29,15 @@ export class InternalServerError extends Error {
   statusCode: number;
   constructor(message: string) {
     super(message);
-    this.name = 'InternalServerError';
+    this.name = "InternalServerError";
     this.statusCode = 500;
+  }
+}
+
+export class ConflictError extends Error {
+  statusCode: number = 409;
+  constructor(message: string) {
+    super(message);
+    this.name = "ConflictError";
   }
 }
