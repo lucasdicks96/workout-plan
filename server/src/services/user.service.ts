4a40b3997b7328a442cd2894f9a6a553a3e75431
Lucas Dicks <lucas1996dicks@gmail.com>
2025-09-23 23:15:41 +0200

    replaced try/catch with customerror

--- server/src/services/user.service.ts
+++ server/src/services/user.service.ts
@@ -1,23 +1,9 @@
 import { hashPassword } from "./auth.service";
-// import { createNewUser } from "../repositories/user.repository";
 import * as userRepository from "../repositories/user.repository";
 
 export async function createUser(email: string, password: string) {
-  try {
-    if (!email || !password) {
-      throw new Error("Email und Passwort sind erforderlich");
-    }
-    const hashedPassword = await hashPassword(password);
-    if (!hashedPassword) {
-      throw new Error("Fehler beim Hashen des Passworts");
-    }
+  const hashedPassword = await hashPassword(password);
+  const user = await userRepository.createUser(email, hashedPassword);
 
-    const user = await userRepository.createUser(email, hashedPassword);
-    if (!user) {
-      throw new Error("Fehler beim Erstellen des Benutzers");
-    }
-    return user;
-  } catch (error) {
-    throw error;
-  }
+  return user;
 }
