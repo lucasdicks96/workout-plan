84867699cc26feb959d98d4bf29b7138b6f8dd8d
Lucas Dicks <lucas1996dicks@gmail.com>
2025-09-23 23:13:14 +0200

    delete comments

--- server/src/types/express/index.d.ts
+++ server/src/types/express/index.d.ts
@@ -1,16 +1,9 @@
-import { User as CustomUser } from "../user.types";
+import { UserWithoutPassword as CustomUser } from "../user.types";
 
-// Erweitere die globalen Typen von Express
 declare global {
   namespace Express {
-    // Erweitere das User-Interface von Passport.
-    // Sage TypeScript, dass Passport's User-Objekt die gleiche Struktur wie dein eigener User-Typ hat.
     export interface User extends CustomUser {}
-
-    // Füge die 'user'-Eigenschaft zum Request-Interface hinzu.
     export interface Request {
-      // Das Fragezeichen '?' macht die Eigenschaft optional, da sie bei
-      // nicht-authentifizierten Anfragen nicht vorhanden ist.
       user?: User;
     }
   }
