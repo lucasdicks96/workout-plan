b050291d9da52da4084525e713d70b03e52ccc3a
Lucas Dicks <lucas1996dicks@gmail.com>
2025-10-08 00:37:33 +0200

    remove comments/divs/unused params

--- client/src/components/Workouts/Workouts.tsx
+++ client/src/components/Workouts/Workouts.tsx
@@ -1,9 +1,8 @@
 import { useCallback, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useAuth } from "../../hooks/useAuth";
+import { useSetTitle } from "../../hooks/useSetTitle";
 import { apiService } from "../../services/apiService";
 import styles from "../../styles/Exercises.module.css";
-import stylesLayout from "../../styles/Layout.module.css";
 import stylesModal from "../../styles/Modal.module.css";
 import { Workout as IWorkout } from "../../types/workouts";
 import PlayPauseButton from "../PlayPauseButton";
@@ -12,17 +11,12 @@ export default function Workout() {
   const [workoutList, setWorkoutList] = useState<IWorkout[] | []>([]);
   const [isLoading, setIsLoading] = useState(true);
   const navigate = useNavigate();
-  const { user } = useAuth();
-  // const id = useRef<string>("");
+
+  useSetTitle("Trainingspläne");
 
   const loadAllWorkouts = useCallback(async () => {
     try {
-      if (!user || user.id === undefined || user.id === null) {
-        console.error("Benutzer ist nicht angemeldet oder hat keine ID.");
-        return;
-      }
       const response = await apiService.getAllWorkouts();
-      // if (Array.isArray(response.data.workouts)) setWorkoutList(response.data.workouts);
       setWorkoutList(response.data.workouts);
     } catch (error) {
       setWorkoutList([]);
@@ -30,14 +24,14 @@ export default function Workout() {
     } finally {
       setIsLoading(false);
     }
-  }, [user]);
+  }, []);
   useEffect(() => {
     loadAllWorkouts();
   }, [loadAllWorkouts]);
 
-  const handleDelete = async (workoutId: number, userId: string) => {
+  const handleDelete = async (workoutId: number) => {
     try {
-      const response = await apiService.deleteWorkout(userId, workoutId);
+      const response = await apiService.deleteWorkout(workoutId);
       loadAllWorkouts();
       console.log(response.data);
     } catch (error) {
@@ -47,25 +41,21 @@ export default function Workout() {
 
   return (
     <>
-      <div className="content">
-        <h2 className={stylesLayout.pageTitle}>Workouts</h2>
-        <WorkoutList
-          isLoading={isLoading}
-          workoutList={workoutList}
-          onDelete={handleDelete}
-        />
-        <div className="button-container">
-          <button className="button" onClick={() => navigate("edit-workouts")}>
-            Bearbeiten
-          </button>
-          <button
-            className="button"
-            onClick={() => navigate("create-workouts")}
-          >
-            Erstellen
-          </button>
-        </div>
+      {/* <div className="content"> */}
+      <WorkoutList
+        isLoading={isLoading}
+        workoutList={workoutList}
+        onDelete={handleDelete}
+      />
+      <div className="button-container">
+        <button className="button" onClick={() => navigate("edit-workouts")}>
+          Bearbeiten
+        </button>
+        <button className="button" onClick={() => navigate("create-workouts")}>
+          Erstellen
+        </button>
       </div>
+      {/* </div> */}
     </>
   );
 }
@@ -79,7 +69,7 @@ export function WorkoutList({
   isLoading: boolean;
   workoutList: IWorkout[];
   onClick?: (workoutId: number) => void;
-  onDelete?: (workoutId: number, userId: string) => void;
+  onDelete?: (workoutId: number) => void;
 }) {
   if (isLoading) {
     return <p>Lade Workouts...</p>;
@@ -89,7 +79,7 @@ export function WorkoutList({
     return <p>Keine Workouts verfügbar.</p>;
   }
   return (
-    <div className={styles.exerciseList}>
+    <>
       {workoutList.map((workout) => (
         <WorkoutCard
           key={workout.id}
@@ -99,7 +89,7 @@ export function WorkoutList({
           onDelete={onDelete}
         />
       ))}
-    </div>
+    </>
   );
 }
 function WorkoutCard({
@@ -111,27 +101,23 @@ function WorkoutCard({
   title: string;
   workoutId: number;
   onClick?: (workoudId: number) => void;
-  onDelete?: (workoutId: number, userId: string) => void;
+  onDelete?: (workoutId: number) => void;
 }) {
   const navigate = useNavigate();
-  const { user } = useAuth();
   const location = window.location.pathname;
   const isEditPage: boolean = location.includes("edit-workouts");
-  let userId: string;
-  if (user) {
-    userId = user.id;
-  }
+
   const onStart = () => {
     localStorage.setItem("startWorkoutId", JSON.stringify(workoutId));
     navigate("start-workouts");
   };
 
   return (
-    <div className="card" onClick={() => onClick?.(workoutId)}>
+    <div className={styles.card} onClick={() => onClick?.(workoutId)}>
       {isEditPage && (
         <button
           className={stylesModal.closeButton}
-          onClick={() => onDelete?.(workoutId, userId)}
+          onClick={() => onDelete?.(workoutId)}
         >
           &times;
         </button>
