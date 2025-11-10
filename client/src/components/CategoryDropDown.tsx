2b1f6ec314fcb4444a508e81522d56d62f0679ee
Lucas Dicks <lucas1996dicks@gmail.com>
2025-11-10 16:57:47 +0100

    init

---
+++ client/src/components/CategoryDropDown.tsx
@@ -0,0 +1,37 @@
+import { useExercises } from "../hooks/useExercises";
+
+export default function CategoryDropdown({
+  selectedCategory,
+  onCategoryChange,
+}: {
+  selectedCategory: number | "Alle";
+  onCategoryChange: (value: number | "Alle") => void;
+}) {
+  const { categoryTree, isCategoryLoading, renderCategoryOptions } =
+    useExercises();
+
+  if (isCategoryLoading) {
+    return <p>Kategorien werden geladen...</p>;
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const val = e.target.value;
+    if (val === "Alle") {
+      onCategoryChange("Alle");
+    } else {
+      const numVal = Number(val);
+      if (!isNaN(numVal)) {
+        onCategoryChange(numVal);
+      } else {
+        console.error("Ungültige Kategorie-ID:", val);
+      }
+    }
+  };
+
+  return (
+    <select className="input" value={selectedCategory} onChange={handleChange}>
+      <option value="Alle">Alle Kategorien</option>
+      {renderCategoryOptions(categoryTree)}
+    </select>
+  );
+}
---
+++ client/src/components/SearchInput.tsx
@@ -0,0 +1,21 @@
+type SearchInputProps = {
+  value: string;
+  onChange: (value: string) => void;
+  placeholder?: string;
+};
+
+export default function SearchInput({
+  value,
+  onChange,
+  placeholder,
+}: SearchInputProps) {
+  return (
+    <input
+      className="input"
+      type="text"
+      value={value}
+      placeholder={placeholder || "Suchen..."}
+      onChange={(e) => onChange(e.target.value)}
+    />
+  );
+}
