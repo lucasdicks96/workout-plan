8917fd0d5bc325fa22c9a556e42bb80b0df395f7
Lucas Dicks <lucas1996dicks@gmail.com>
2025-11-10 16:56:34 +0100

    moved buttons

--- client/src/components/Buttons/EditButton.tsx
+++ client/src/components/Buttons/EditButton.tsx
@@ -1,4 +1,4 @@
-import styles from "../styles/Button.module.css";
+import styles from "../../styles/Button.module.css";
 type EditButtonProps = {
   onEdit: () => void;
   className?: string;
--- client/src/components/Buttons/ReturnButton.tsx
+++ client/src/components/Buttons/ReturnButton.tsx
@@ -1,4 +1,4 @@
-import styles from "../styles/Button.module.css";
+import styles from "../../styles/Button.module.css";
 
 type ReturnButtonProps = {
   onBack: () => void;
