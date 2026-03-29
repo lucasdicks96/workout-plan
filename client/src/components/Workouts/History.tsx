66d18c0df023ad3fd11f20f82545950f30ddb9a0
Lucas Dicks <lucas1996dicks@gmail.com>
2026-03-29 22:47:51 +0200

    feat: Fügt Bearbeitungsbutton und Datumsformatierung im Verlauf hinzu

--- client/src/components/Workouts/History.tsx
+++ client/src/components/Workouts/History.tsx
@@ -1,12 +1,16 @@
 import { isAxiosError } from "axios";
 import { useCallback, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../hooks/useAuth";
 import { useSetTitle } from "../../hooks/useSetTitle";
 import { apiService } from "../../services/apiService";
-import { FinishedWorkout } from "../../types/workouts";
+import styles from "../../styles/Exercises.module.css";
+import stylesButton from "../../styles/Button.module.css";
+import { CompletedWorkout } from "../../types/workouts";
+import EditButton from "../Buttons/EditButton";
 
 export default function History() {
-  const [workouts, setWorkouts] = useState<FinishedWorkout[] | null>([]);
+  const [workouts, setWorkouts] = useState<CompletedWorkout[] | null>([]);
   const { user } = useAuth();
 
   useSetTitle("Verlauf");
@@ -16,7 +20,7 @@ export default function History() {
       if (!user) return;
       const response = await apiService.getCompletedWorkouts();
       console.log("Response Data History ", response.data);
-      setWorkouts(response.data.exercises);
+      setWorkouts(response.data.workouts);
     } catch (error) {
       if (isAxiosError(error)) {
         if (
@@ -43,22 +47,30 @@ export default function History() {
       {!workouts || workouts.length === 0 ? (
         <div>Bisher wurden keine Workouts absolviert</div>
       ) : (
-        <>
+        <div className={styles.exerciseList}>
           {workouts.map((workout) => (
             <HistoryItem key={workout.id} workout={workout} />
           ))}
-        </>
+        </div>
       )}
     </>
     // </div>
   );
 }
 
-const HistoryItem = ({ workout }: { workout: FinishedWorkout }) => {
+const HistoryItem = ({ workout }: { workout: CompletedWorkout }) => {
+  const navigate = useNavigate();
+  const date = new Date(workout.startTime);
+
   return (
-    <div>
-      {workout.title}
-      {workout.date}
+    <div className={styles.card}>
+      <h3 className={styles.workoutCardTitle}>{workout.title}</h3>
+      {date.toLocaleDateString()} - {date.toLocaleTimeString()}
+      <EditButton
+        // Change the route to pass the specific completed workout ID
+        onEdit={() => navigate(`/history/edit/${workout.id}`)}
+        className={`${stylesButton.right} ${stylesButton.buttonRounded}`}
+      />
     </div>
   );
 };
