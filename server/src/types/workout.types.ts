f5537d2af1f95488918c08ef425f81f827fd4afc
Lucas Dicks <lucas1996dicks@gmail.com>
2026-05-04 01:02:35 +0200

    add interface flattenedCompleteWorkout

--- server/src/types/workout.types.ts
+++ server/src/types/workout.types.ts
@@ -30,3 +30,20 @@ export interface WorkoutExerciseSets {
   weight: number;
   repetitions: number;
 }
+
+export interface FlattenedCompletedWorkout {
+  workout_id: string;
+  user_id: string;
+  workout_plan_id: number;
+  plan_title: string;
+  duration_seconds: number;
+  start_time: Date;
+  end_time: Date;
+  pause_seconds: number;
+  exercise_id: number;
+  title: string;
+  display_order: number;
+  set_number: number;
+  weight: number;
+  repetitions: number;
+}
