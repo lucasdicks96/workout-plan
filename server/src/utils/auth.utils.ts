b54ec480021bfdcf64da7ef6b36ba18b1025509c
Lucas Dicks <lucas1996dicks@gmail.com>
2026-05-07 00:28:41 +0200

    modified to work with generics

--- server/src/utils/auth.utils.ts
+++ server/src/utils/auth.utils.ts
@@ -1,16 +1,33 @@
-import { RequestHandler } from "express";
-import { Request, Response } from "express";
+import { RequestHandler, Request, Response } from "express";
 import { UserWithoutPassword } from "../types/user.types";
 
-type AuthenticatedRequest = Request & { user: UserWithoutPassword };
+// 1. Wir geben deinem Typen die gleichen Generics wie Express (P = Params, ResBody = Response, ReqBody = RequestBody, ReqQuery = Query)
+// Die = any sorgen dafür, dass sie optional sind, genau wie beim Original.
+export type AuthenticatedRequest<
+  P = any,
+  ResBody = any,
+  ReqBody = any,
+  ReqQuery = any,
+> = Request<P, ResBody, ReqBody, ReqQuery> & { user: UserWithoutPassword };
 
-// castet Request zu AuthenticatedRequest
-export function authenticatedHandler(
-  handler: (req: AuthenticatedRequest, res: Response) => Promise<void>
-): RequestHandler {
+// 2. Wir machen die Wrapper-Funktion generisch, damit sie die Typen aus dem Controller "auffängt" und weiterleitet
+export function authenticatedHandler<
+  P = any,
+  ResBody = any,
+  ReqBody = any,
+  ReqQuery = any,
+>(
+  handler: (
+    req: AuthenticatedRequest<P, ResBody, ReqBody, ReqQuery>,
+    res: Response<ResBody>,
+  ) => Promise<void>,
+): RequestHandler<P, ResBody, ReqBody, ReqQuery> {
   return async (req, res, next) => {
     try {
-      await handler(req as AuthenticatedRequest, res);
+      await handler(
+        req as AuthenticatedRequest<P, ResBody, ReqBody, ReqQuery>,
+        res,
+      );
     } catch (error) {
       next(error);
     }
