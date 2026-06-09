ac4800cfed30f9db7e7190d37877cad4687ac40a
Lucas Dicks <lucas1996dicks@gmail.com>
2026-06-10 00:47:54 +0200

    changed throw error to next(error)

--- server/src/middlewares/isAuthenticated.ts
+++ server/src/middlewares/isAuthenticated.ts
@@ -7,15 +7,15 @@ const userIdSchema = z.uuid();
 export function isAuthenticated(
   req: Request,
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ) {
   if (!req.isAuthenticated() || !req.user) {
-    throw new UnauthorizedError("Nicht authentifiziert");
+    return next(new UnauthorizedError("Nicht authentifiziert"));
   }
 
   const parsedUserId = userIdSchema.safeParse(req.user.id);
   if (!parsedUserId.success) {
-    throw new UnauthorizedError("Ungültige User-ID in Session");
+    return next(new UnauthorizedError("Ungültige User-ID in Session"));
   }
 
   next();
