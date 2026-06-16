6c0af082a24adaa37a1fa0d5cf2df06e9829f9fd
Lucas Dicks <lucas1996dicks@gmail.com>
2026-06-16 23:35:04 +0200

    add createWorkoutPlan and createCompletedWorkout

--- server/__tests__/testHelpers.ts
+++ server/__tests__/testHelpers.ts
@@ -1,9 +1,9 @@
 // __tests__/testHelpers.ts
-import request from "supertest";
-import app from "../src/index";
-import pool from "../src/config/db";
 import bcrypt from "bcrypt";
 import crypto from "crypto"; // <-- Nativ in Node.js, keine Installation nötig!
+import request from "supertest";
+import pool from "../src/config/db";
+import app from "../src/index";
 
 export async function createAndLoginTestUser() {
   const password = "testpassword123";
@@ -39,4 +39,77 @@ export async function createAndLoginTestUser() {
 
   // Wir geben die ID (für spätere Abfragen) und den Cookie zurück
   return { userId, cookie };
+}
+
+export async function createTestWorkoutPlan({
+  workoutId,
+  userId,
+  title,
+  exerciseId,
+  displayOrder,
+  setNumber,
+  repetitions,
+  weight,
+}: {
+  workoutId: number;
+  userId: string;
+  title: string;
+  exerciseId: number;
+  displayOrder: number;
+  setNumber: number;
+  repetitions: number;
+  weight: number;
+}) {
+  await pool.query(
+    "INSERT INTO workout_plans (id, user_id, title) VALUES ($1, $2, $3)",
+    [workoutId, userId, title],
+  );
+
+  const peResult = await pool.query(
+    "INSERT INTO plan_exercises (workout_plan_id, exercise_id, display_order) VALUES ($1, $2, $3) RETURNING id",
+    [workoutId, exerciseId, displayOrder],
+  );
+
+  await pool.query(
+    "INSERT INTO plan_sets (plan_exercise_id, set_number, repetitions, weight) VALUES ($1, $2, $3, $4)",
+    [peResult.rows[0].id, setNumber, repetitions, weight],
+  );
+}
+
+export async function createTestCompletedWorkout({
+  workoutId,
+  userId,
+  title,
+  startTime,
+  endTime,
+  duration,
+  pause,
+  exerciseId,
+  setNumber,
+  repetitions,
+  weight,
+}: {
+  workoutId: number;
+  userId: string;
+  title: string;
+  startTime: Date;
+  endTime: Date;
+  duration: number;
+  pause: number;
+  exerciseId: number;
+  setNumber: number;
+  repetitions: number;
+  weight: number;
+}): Promise<string> {
+  const res = await pool.query(
+    "INSERT INTO completed_workouts (user_id, workout_plan_id, title, start_time, end_time, duration_seconds, pause_seconds) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id",
+    [userId, workoutId, title, startTime, endTime, duration, pause],
+  );
+
+  await pool.query(
+    "INSERT INTO completed_sets (completed_workout_id, exercise_id, set_number, repetitions, weight) VALUES ($1, $2, $3, $4, $5)",
+    [res.rows[0].id, exerciseId, setNumber, repetitions, weight],
+  );
+
+  return res.rows[0].id;
 }
