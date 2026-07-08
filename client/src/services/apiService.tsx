b3dc6084cfb6eabfea1b7db2873750a2bb24d081
Lucas Dicks <lucas1996dicks@gmail.com>
2026-07-08 20:11:39 +0200

    add baseUrl variable import for docker

--- client/src/services/apiService.tsx
+++ client/src/services/apiService.tsx
@@ -10,7 +10,7 @@ export interface ApiResponse<T = void> {
 }
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
