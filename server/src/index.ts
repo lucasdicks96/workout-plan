29a009462e57575e5073d105ffb9968e26ad3de5
Lucas Dicks <lucas1996dicks@gmail.com>
2026-07-15 18:30:12 +0200

    add ratelimit

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,11 +10,31 @@ import errorHandler from "./middlewares/error";
 import userRoute from "./routes/auth.route";
 import exerciseRoute from "./routes/exercise.route";
 import workoutRoute from "./routes/workout.route";
+import rateLimit from "express-rate-limit";
 env.config();
 
 const app = express();
 
 const PostgresqlStore = pgSession(session);
+
+const authLimiter = rateLimit({
+  windowMs: 60 * 60 * 1000,
+  max: 5,
+  message: {
+    error:
+      "Zu viele Registrierungs- oder Login-Versuche. Bitte versuchen Sie es in einer Stunde erneut.",
+  },
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: {
+    error: "Zu viele Anfragen. Versuche es später erneut.",
+  },
+});
 
 app.use(
   cors({
@@ -61,6 +81,11 @@ app.use(session(sessionConfig));
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+app.use("/user/register", authLimiter);
+app.use("/user/login", authLimiter);
+app.use("/workout", apiLimiter);
+app.use("/exercise", apiLimiter);
 
 app.use("/user", userRoute);
 app.use("/exercise", exerciseRoute);
